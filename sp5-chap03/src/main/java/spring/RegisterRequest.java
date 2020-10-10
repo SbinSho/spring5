5d8720a20d12ed34aa5c4782dc754e2a958d7bde
SbinSho <suho0232@gmail.com>
2020-10-10 16:54:17 +0900

    assembler 클래스 생성, 의존주입을 위한 클래스

--- sp5-chap03/src/main/java/spring/RegisterRequest.java
+++ sp5-chap03/src/main/java/spring/RegisterRequest.java
@@ -4,38 +4,42 @@ public class RegisterRequest {
 
 	private String email;
 	private String password;
-	private String conformpassword;
+	private String confirmPassword;
 	private String name;
-	
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getConformpassword() {
-		return conformpassword;
+
+	public String getConfirmPassword() {
+		return confirmPassword;
 	}
-	public void setConformpassword(String conformpassword) {
-		this.conformpassword = conformpassword;
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public boolean isPasswordEqualToConformPassword() {
-		return password.equals(conformpassword);
-		
+
+	public boolean isPasswordEqualToConfirmPassword() {
+		return password.equals(confirmPassword);
 	}
-	
-	
 }
