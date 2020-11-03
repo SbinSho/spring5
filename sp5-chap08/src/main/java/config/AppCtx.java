f0b050647a501583ba497861027ae62c3f781e6f
SbinSho <suho0232@gmail.com>
2020-11-03 20:48:29 +0900

    MemberDao test

--- sp5-chap08/src/main/java/config/AppCtx.java
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -5,6 +5,8 @@ import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import spring.MemberDao;
+
 @Configuration
 public class AppCtx {
 
@@ -14,14 +16,17 @@ public class AppCtx {
 		DataSource ds = new DataSource();
 		ds.setDriverClassName("com.mysql.jdbc.Driver");
 		ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
-		ds.setUrl("spring5");
+		ds.setUsername("spring5");
 		ds.setPassword("spring5");
 		ds.setInitialSize(2);
 		ds.setMaxActive(10);
 		return ds;
 	}
 	
-	
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao(dataSource());
+	}
 	
 	
 }
