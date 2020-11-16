7073bce215e0946f65450111faadf75aa23cb4de
SbinSho <suho0232@gmail.com>
2020-11-16 20:20:20 +0900

    chap13 @ControllerAdvice

--- sp5-chap14/src/main/java/controller/MemberDetailController.java
+++ sp5-chap14/src/main/java/controller/MemberDetailController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -28,5 +30,15 @@ public class MemberDetailController {
 		return "member/memberDetail";
 	}
 
+	@ExceptionHandler(TypeMismatchException.class)
+	public String handleTypeMismatchException() {
+		return "member/invalidId";
+	}
+	
+	@ExceptionHandler(MemberNotFoundException.class)
+	public String handleNotFoundException() {
+		return "member/noMember";
+	}
+	
 
 }
